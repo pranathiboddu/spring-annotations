fd22e42d07015a125c6edcb88145b92c130487c8
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 09:52:39 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -6,9 +6,23 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+        //using application context
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(MovieConfiguration.class);
         Movie movie = annotationConfigApplicationContext.getBean("movie", Movie.class);
         movie.display();
+         //using bean factory
+        XmlBeanFactory xmlBeanFactory=new XmlBeanFactory(new FileSystemResource("/home/pranathi/spring-xml-demo/src/main/resources/beans.xml"));
+        Movie movie2=xmlBeanFactory.getBean("movie",Movie.class);
+        movie2.display();
+
+        //using beandefinitionregistry and beandefinitionreader
+        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
+        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beanDefinitionReader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
+        Movie movie3 =((DefaultListableBeanFactory)beanDefinitionRegistry).getBean("movie",Movie.class);
+        movie3.display();
+     }
+
 
 
     }
