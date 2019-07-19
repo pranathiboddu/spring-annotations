f9366a72c4740a732a693a5f4d9232f3e8d19ddf
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 10:02:39 +0530

    Update MovieConfiguration.java

--- src/main/java/com/stackroute/domain/MovieConfiguration.java
+++ src/main/java/com/stackroute/domain/MovieConfiguration.java
@@ -4,9 +4,9 @@ package com.stackroute.domain;
 import org.springframework.context.annotation.*;
 
 
-@Configuration
-@ComponentScan(basePackages="com.stackroute.domain")
-@PropertySource("classpath:actorinfo.properties")
+@Configuration //used configuration
+@ComponentScan(basePackages="com.stackroute.domain") //component scan for scanning packages
+@PropertySource("classpath:actorinfo.properties") //path for properties info
 public class MovieConfiguration
 {
 
