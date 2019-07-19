57e971029b555c5c69c1a79c6169cf7768c56775
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 10:05:57 +0530

    Update MovieConfiguration.java

--- src/main/java/com/stackroute/domain/MovieConfiguration.java
+++ src/main/java/com/stackroute/domain/MovieConfiguration.java
@@ -6,15 +6,16 @@ import com.stackroute.demo.BeanPostProcessorDemoBean;
 import org.springframework.context.annotation.*;
 
 
-@Configuration
-@ComponentScan(basePackages="com.stackroute.demo")
-@PropertySource("classpath:actorinfo.properties")
+@Configuration //used configuration
+@ComponentScan(basePackages="com.stackroute.domain") //component scan for scanning packages
+//@PropertySource("classpath:actorinfo.properties") //path for properties info
 public class MovieConfiguration {
 
    /* @Bean(name = "beanlifecycledemobean",initMethod = "customInit",destroyMethod = "customDestroy")
     public BeanLifecycleDemoBean getBeanLifeCycleDemoBean() {
         return new BeanLifecycleDemoBean();
 */
+   //created bean object for beanpostprocessor
         @Bean(name = "beanpostprocessordemo",initMethod = "initBean" ,destroyMethod = "destroyBean")
         public BeanPostProcessorDemoBean getBeanPostProcessorDemoBean()
         {
