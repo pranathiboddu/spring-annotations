759b70a7348285a279fad14b7672d581594ec09a
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 09:59:17 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component //declared component to create a bean for movie
 public class Movie {
-    @Autowired
+    @Autowired //used @autowire
     private Actor actor;
 
     public void display() {
