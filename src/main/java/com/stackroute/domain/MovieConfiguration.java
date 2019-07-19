8e8c7ea26dbe4be699d1220c748b42e964de5304
Boddu Pranathi <37667750+pranathiboddu@users.noreply.github.com>
2019-07-19 10:03:34 +0530

    Update MovieConfiguration.java

--- src/main/java/com/stackroute/domain/MovieConfiguration.java
+++ src/main/java/com/stackroute/domain/MovieConfiguration.java
@@ -4,18 +4,18 @@ package com.stackroute.domain;
 import org.springframework.context.annotation.*;
 
 
-@Configuration
-@ComponentScan(basePackages="com.stackroute.domain")
-//@PropertySource("classpath:actorinfo.properties")
+@Configuration //used configuration
+@ComponentScan(basePackages="com.stackroute.domain") //component scan for scanning packages
+//@PropertySource("classpath:actorinfo.properties") //path for properties info
 public class MovieConfiguration {
 
-    @Bean("actor")
+    @Bean("actor") //created a bean object using @Bean for actor class
     public Actor actor1() {
         Actor actor = new Actor("Shahid Kapoor", "male", 35);
         return actor;
     }
 
-    @Bean("movie1")
+    @Bean("movie1")//created a bean object using @Bean for movie class
     @Scope(value="prototype")
     public Movie movie1() {
         return new Movie(actor1());
